e3e6b4e1a70cfa111c719db830a4f19f257405db
Adrian Ray LeDeaux <138169736+TheW0NK@users.noreply.github.com>
2024-10-11 12:20:07 -0600

    Update pwr.c

--- PyOS/src/pwr.c
+++ PyOS/src/pwr.c
@@ -5,8 +5,10 @@
 // Function to shut down the kernel
 void shutdown_kernel() {
     display_puts("Shutting down...\n");
-    // Add architecture-specific shutdown code here
+    
+    outw(0xB004, 0x2000);
     while (1) {
+    display_puts("VHK12: ACPI FAILED! INITIATING FALLBACK\n);
         __asm__ __volatile__("hlt");
     }
 }
@@ -23,15 +25,18 @@ void restart_kernel() {
 // Function to put the kernel to sleep
 void sleep_kernel() {
     display_puts("Sleeping...\n");
-    // Add architecture-specific sleep code here
+    outw(0x64, 0xFE);
+        
     while (1) {
+        display_puts("VHK006 ACPI FAILED!\n");
         __asm__ __volatile__("hlt");
     }
 }
 
 // Function to hibernate the kernel
 void hibernate_kernel() {
-    display_puts("Hibernating...\n");
+    display_puts("VHK001: savestate.sys does not exist.\n");
+    outw(0xB004, 0x2000);
     // Add architecture-specific hibernate code here
     while (1) {
         __asm__ __volatile__("hlt");
